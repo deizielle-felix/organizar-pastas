da9b08b0d36a2b03a6288b9ce10330c145aff60b
Deizielle Felix <142148960+deizielle-felix@users.noreply.github.com>
2024-03-16 19:47:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,19 @@ import os
 import shutil
 
 
-dir_dest = {'Pasta de imagens': ['.jpg', '.png', '.svg'],
-             'Pasta de videos': ['mp4'],
-             'Pasta de PDF': ['.pdf']}
+dir_dest = {'Imagens': ['.jpg', '.png', '.svg'],
+             'Vídeos': ['.mp4'],
+             'PDF': ['.pdf']
+             }
 
-path = 'path/para/pasta/desorganizada'
+user = os.environ['USERNAME']
+path = f"/home/{user}/Downloads" # caminho para a pasta desorganizada
 archives_dir = os.listdir(path=path)
 
 for archive in archives_dir:
     for directory, file_type in dir_dest.items():
         if any(ext in archive for ext in file_type):
-            shutil.move(f'{path}/{archive}', f'{path}/{directory}')
+            destination = f'/home/{user}/{directory}'
+            origin = f'{path}/{archive}'
+            shutil.move(origin, destination)
+
